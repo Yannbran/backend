aaeeb8839eb356eb26a89a889e55c28a78a5aab5
Yann <yannbran@gmail.com>
2024-07-24 15:47:39 +0200

    créé des tokens d auth

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,17 +1,15 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
+
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', stuffCtrl.createThing);
-
-  router.put('/:id', stuffCtrl.modifyThing);
-
-  router.delete('/:id', stuffCtrl.deleteThing);
-
-  router.get('/:id', stuffCtrl.getOneThing);
-
-  router.get('/', stuffCtrl.getAllThing);
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, stuffCtrl.createThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id',auth, stuffCtrl.deleteThing);
+router.get('/:id',auth, stuffCtrl.getOneThing);
 
 module.exports = router;
